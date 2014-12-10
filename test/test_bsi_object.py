7004748b45fe4636f9d9918bdbcb9878091c98c4
jshou <joshuahou@gmail.com>
2014-12-09 20:30:37 -0800

    bsi objects are printable

--- test/test_bsi_object.py
+++ test/test_bsi_object.py
@@ -8,3 +8,11 @@ class TestBsiObject(unittest.TestCase):
     def test_bsi_object_can_add_and_get_key_value_pairs(self):
         self.bsi_o.add('key', 'value')
         self.assertEqual(self.bsi_o.get('key'), 'value')
+
+    def test_bsi_object_to_string(self):
+        self.bsi_o.add('k1', 'v1')
+        self.bsi_o.add('k2', 'v2')
+
+        str_obj = 'k1 = v1\nk2 = v2'
+
+        self.assertEqual(str(self.bsi_o), str_obj)
