19ebf6e6d9276722f1932289f78052176aa0dc7c
jshou <joshuahou@gmail.com>
2014-12-09 17:58:40 -0800

    setup.py for bsi package

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='bsi',
+    version='0.1',
+    description='Python module for reading/editing/writing .bsi files',
+    url='https://github.com/jshou/bsi',
+    author='Joshua Hou',
+    author_email='joshuahou@gmail.com',
+    packages=['bsi'],
+    zip_safe=False
+)
