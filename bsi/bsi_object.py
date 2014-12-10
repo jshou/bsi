eea76dae7abd3d05e50cd76ec5216d7d90aceafc
jshou <joshuahou@gmail.com>
2014-12-09 22:52:44 -0800

    multi nested printing

--- bsi/bsi_object.py
+++ bsi/bsi_object.py
@@ -12,5 +12,18 @@ class BsiObject:
         return self.dict[k]
 
     def __str__(self):
-        pairs = ["{} = {}".format(pair[0], str(pair[1])) for pair in self.dict.items()]
+        pairs = []
+
+        for pair in self.dict.items():
+            key = pair[0]
+            if isinstance(pair[1], BsiObject):
+                val = pair[1].nested_str()
+            else:
+                val = str(pair[1])
+            pairs.append("{} = {}".format(key, val))
         return '\n'.join(pairs)
+
+    def nested_str(self):
+        lines = str(self).split('\n')
+        indented_lines = '\n'.join(['\t' + line for line in lines])
+        return '{\n' + indented_lines + '\n}'
--- test/test_bsi_object.py
+++ test/test_bsi_object.py
@@ -23,3 +23,24 @@ class TestBsiObject(unittest.TestCase):
         str_obj = 'k1 = [1 2 3]'
 
         self.assertEqual(str(self.bsi_o), str_obj)
+
+    def test_bsi_object_to_string_nested_objects(self):
+        zombie1 = bsi.BsiObject()
+        zombie1.set('name', bsi.BsiString("Dead Bob"))
+
+        nested = bsi.BsiObject()
+        nested.set('zombie1', zombie1)
+        nested.set('zombie2', bsi.BsiString('double dead'))
+
+        self.bsi_o.set('k1', 2)
+        self.bsi_o.set('zombies', nested)
+
+        str_obj = '''k1 = 2
+zombies = {
+\tzombie1 = {
+\t\tname = "Dead Bob"
+\t}
+\tzombie2 = "double dead"
+}'''
+
+        self.assertEqual(str(self.bsi_o), str_obj)
