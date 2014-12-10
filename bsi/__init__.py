19ac44f39d2c55fc81e3ccf62db9bbf7516a4473
jshou <joshuahou@gmail.com>
2014-12-09 20:18:14 -0800

    external facing API for parsing strings and returning a BsiObject

--- bsi/__init__.py
+++ bsi/__init__.py
@@ -1,4 +1,8 @@
-from .bsi import BSI
 from .bsi_lexer import bsi_lexer
 from .bsi_object import BsiObject
 from .bsi_parser import bsi_parser
+
+parser = bsi_parser
+
+def bsi_parse(data):
+    return bsi_parser.parse(data)
